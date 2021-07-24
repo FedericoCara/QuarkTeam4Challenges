2d65f857ba1ebf644cfe9e01ebc3b50c4f2928be
Federico Cara <potter_federico@hotmail.com>
2021-07-24 17:09:25 -0300

    commit inicial

---
+++ QuarkTeam4Challenges/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace QuarkTeam4Challenges
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
