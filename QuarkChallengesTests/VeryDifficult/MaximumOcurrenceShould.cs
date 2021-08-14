4d7ea82869acbd3fb86c618407b1221ea648d16e
Federico <potter_federico@hotmail.com>
2021-08-14 19:40:07 -0300

    MaximumOcurrence exercise added.

---
+++ QuarkChallengesTests/VeryDifficult/MaximumOcurrenceShould.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuarkTeam4Challenges.VeryDificult;
+
+namespace QuarkChallengesTests
+{
+    /*
+     Given a string text. Write a function that returns the character with the highest frequency. 
+    If more than 1 character has the same highest frequency, return all those characters as a comma separated string. 
+    If there is no repetition in characters, return "No Repetition".
+     */
+
+    [TestClass]
+    public class MaximumOcurrenceShould
+    {
+
+        [TestMethod]
+        public void CheckManyOcurrencesOfOneLetter() {
+            //Given
+            string givenString = "Computer Science";
+
+            //When
+            string result = MaximumOcurrence.MaxOccur(givenString);
+
+            //Then
+            Assert.AreEqual("e", result);
+        }
+
+        [TestMethod]
+        public void CheckManyOcurrencesOfManyLetters() {
+            //Given
+            string givenString = "system admin";
+
+            //When
+            string result = MaximumOcurrence.MaxOccur(givenString);
+
+            //Then
+            Assert.AreEqual("m, s", result);
+        }
+
+        [TestMethod]
+        public void CheckNoRepetitions() {
+            //Given
+            string givenString = "Edabit";
+
+            //When
+            string result = MaximumOcurrence.MaxOccur(givenString);
+
+            //Then
+            Assert.AreEqual(MaximumOcurrence.NO_REPETITION, result);
+        }
+
+        [TestMethod]
+        public void CheckEmptySpaceOcurrences() {
+            //Given
+            string givenString = "the quick brown fox jumps over the lazy dog";
+
+            //When
+            string result = MaximumOcurrence.MaxOccur(givenString);
+
+            //Then
+            Assert.AreEqual(" ", result);
+        }
+
+        [TestMethod]
+        public void CheckEmptySpaceAndLetterOcurrences() {
+            //Given
+            string givenString = " a a";
+
+            //When
+            string result = MaximumOcurrence.MaxOccur(givenString);
+
+            //Then
+            Assert.AreEqual("a,  ", result);
+        }
+
+    }
+}
---
+++ QuarkTeam4Challenges/VeryDifficult/MaximumOcurrence.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace QuarkTeam4Challenges.VeryDificult
+{
+    public class MaximumOcurrence
+    {
+        public const string NO_REPETITION = "No Repetition";
+
+        public static string MaxOccur(string givenString) {
+            Dictionary<char, int> ocurrencesDict = new Dictionary<char, int>();
+            int maxOcurrence = 0;
+            List<char> maxOcurrenceLetter = new List<char>();
+
+            foreach (char letter in givenString) {
+                SumOcurrenceCount(ocurrencesDict, letter);
+                maxOcurrence = UpdateMaxOcurrences(ocurrencesDict, maxOcurrence, maxOcurrenceLetter, letter);
+            }
+
+            maxOcurrenceLetter.Reverse();
+            return maxOcurrence <= 1 ? NO_REPETITION : string.Join(", ", maxOcurrenceLetter);
+        }
+
+        private static void SumOcurrenceCount(Dictionary<char, int> ocurrencesDict, char letter) {
+            if (ocurrencesDict.ContainsKey(letter)) {
+                ocurrencesDict[letter]++;
+            } else {
+                ocurrencesDict.Add(letter, 1);
+            }
+        }
+
+        private static int UpdateMaxOcurrences(Dictionary<char, int> ocurrencesDict, int maxOcurrence, List<char> maxOcurrenceLetter, char letter) {
+            if (ocurrencesDict[letter] > maxOcurrence) {
+                maxOcurrenceLetter.Clear();
+                maxOcurrenceLetter.Add(letter);
+                maxOcurrence = ocurrencesDict[letter];
+            } else if (ocurrencesDict[letter] == maxOcurrence) {
+                maxOcurrenceLetter.Add(letter);
+            }
+
+            return maxOcurrence;
+        }
+    }
+}
